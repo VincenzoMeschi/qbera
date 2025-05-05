977ae866ee8bbbe44987856df09190235bb52ec1
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-05-04 21:17:51 -0400

    Remove border

--- src/Header/LowImpact/Component.tsx
+++ src/Header/LowImpact/Component.tsx
@@ -18,7 +18,7 @@ export const LowImpactHeader: React.FC<LowImpactHeaderProps> = (props) => {
 					<Nav />
 					<div className="absolute bottom-0 left-0 bg-background flex pl-16 w-full md:w-[37rem] xl:w-1/2 h-32 md:rounded-tr-[2.5rem] z-10 " />
 					<div
-						className="absolute w-10 h-10 left-0 bottom-32 bg-transparent shadow-[0_0_0_10rem_#F0F0F0,0_0_0_0_#F0F0F0] rounded-bl-[2.5rem] z-10 border-red-500 border"
+						className="absolute w-10 h-10 left-0 bottom-32 bg-transparent shadow-[0_0_0_10rem_#F0F0F0,0_0_0_0_#F0F0F0] rounded-bl-[2.5rem] z-10"
 						style={{ clipPath: "inset(0)" }}></div>
 					<div
 						className="absolute w-10 h-10 bottom-32 right-0 md:left-[37rem] xl:left-1/2 md:bottom-0 bg-transparent shadow-[0_0_0_10rem_#F0F0F0,0_0_0_0_#F0F0F0] rounded-br-[2.5rem] md:rounded-bl-[2.5rem] md:rounded-br-none z-10"
