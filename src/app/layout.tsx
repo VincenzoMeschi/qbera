d28a51795c54c919aadfbe7f386ca4c7edd6ca86
Vincenzo Meschi <119996421+VincenzoMeschi@users.noreply.github.com>
2026-03-03 13:21:13 -0800

    Updated layout.tsx file with new content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,52 @@
-import Script from 'next/script';
+import type { Metadata } from "next";
+import { Roboto, Anton } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
-<Script>
-  {`window.google_ads_conversion_id = 'YOUR_CONVERSION_ID';`
-  `window.google_ads_phone_conversion_number = 'YOUR_PHONE_NUMBER';`
-  // Add any additional tracking code here
-}</Script>
+import PlausibleProvider from "next-plausible";
+
+const roboto = Roboto({
+	variable: "--font-roboto",
+	subsets: ["latin"],
+});
+
+const anton = Anton({
+	variable: "--font-anton",
+	subsets: ["latin"],
+	weight: ["400"],
+});
+
+export const metadata: Metadata = {
+	title: "QB Era | Best Quarterback and Wide Receiver Training in Chicago",
+	description:
+		"Quarterback and Wide Receiver Training Academy based in Chicago, Illinois, and Dallas, Texas. Take your training to the next level. Welcome to the new era of quarterback training.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{  
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<GoogleTagManager gtmId="AW-17332557195" />
+			<Script
+			strategy="afterInteractive"
+			dangerouslySetInnerHTML={{
+				__html: `
+				gtag('config', 'AW-17332557195/KbDaCJ3vpusbEIur58hA', {
+					'phone_conversion_number': '(630) 290-1120'
+				});
+				`,
+			}}
+			/>
+			<PlausibleProvider domain="qb-era.com">
+				<body
+					className={`${roboto.variable} ${anton.variable} antialiased`}>  
+					{children}
+				</body>
+			</PlausibleProvider>
+		</html>
+	);
+}
